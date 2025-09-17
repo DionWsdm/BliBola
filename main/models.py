5de59dfd9669beacd8d63d8e1b76ba6510499c6c
DionWsdm <dionwisdom29@gmail.com>
2025-09-17 12:14:24 +0700

    fix

--- main/models.py
+++ main/models.py
@@ -12,7 +12,7 @@ class Product(models.Model):
         ("merch", "Merch"), 
         ("tools", "Tools")
         ]
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id = models.AutoField(primary_key=True, editable=False)
     name = models.CharField(max_length=255)
     price = models.IntegerField()
     description = models.TextField()
